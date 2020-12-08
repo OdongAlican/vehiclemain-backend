7163ae4eb0900869cc9bc99809571c2d9e445c1d
OdongAlican <sandieo.2020@gmail.com>
2020-12-09 01:41:42 +0300

    update configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,6 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.assets.js_compressor = Uglifier.new(:harmony => true)
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -23,8 +22,10 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+  config.assets.js_compressor = Uglifier.new(:harmony => true)
+
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
