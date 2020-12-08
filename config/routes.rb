2777bfa16cb9d544d7cdf2b1035f0db2ade76f96
OdongAlican <sandieo.2020@gmail.com>
2020-12-08 22:43:12 +0300

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+ 
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   # Routes Root 
+  root 'welcome#index' 
+ 
+  devise_for :users, skip: [:registrations, :sessions]
+
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      devise_for :users, controllers: { sessions: 'api/v1/sessions', registrations: 'api/v1/registrations' } 
+
+      # Users routes
+      resources :users
+      resources :vehicles
+      get 'show_vehicle_by_user', to: "vehicles#show_vehicle_by_user"
+
+    end
+  end
+end
+
+
+
